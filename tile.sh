2957631c7e4030cdc0a51ff8cc197580ab295e81
hovercats <hovercatswithlasereyes@protonmail.com>
2022-09-28 08:43:45 +0200

    tile.sh: increase master size

--- tile.sh
+++ tile.sh
@@ -6,7 +6,7 @@
 # default values for gaps and master area
 PANEL=${PANEL:-0}
 GAP=${GAP:-20}
-MASTER=${MASTER:-900}
+MASTER=${MASTER:-1000}
 
 # get current window id and its borderwidth
 PFW=$(pfw)
